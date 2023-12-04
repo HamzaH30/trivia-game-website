24180c2edb546159ec427282962687b20b1314b2
Hamza Haque <hamzahaque30@gmail.com>
2023-12-04 13:12:24 -0600

    added the main page code

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import WelcomePage from "./welcome-page";
+import GamePage from "./game-page";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <WelcomePage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/welcome-page.jsx
@@ -0,0 +1,27 @@
+export default function WelcomePage() {
+  return (
+    <section className="welcome-page-description">
+      <h1>Brain Battle Royale</h1>
+      <p>
+        Welcome to <span className="blue-text">Brain Battle Royale</span>, the
+        ultimate trivia challenge that tests your knowledge and quick thinking!
+      </p>
+      <br />
+      <p>
+        Dive into a world of facts and fun, where players compete head-to-head
+        to answer intriguing questions on a wide range of topics.
+      </p>
+      <br />
+      <p>
+        <span className="green-text">Sharpen your mind,</span>{" "}
+        <span className="amber-text">outwit your opponents,</span>{" "}
+        <span className="orange-text">
+          and rise to the top in this thrilling battle of wits!
+        </span>
+      </p>
+      <div className="play-btn-container">
+        <button className="play-btn">Play Now</button>
+      </div>
+    </section>
+  );
+}
