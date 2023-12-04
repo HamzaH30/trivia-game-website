31cc1ee38d713f26d8c223811535786bd7593993
Hamza Haque <hamzahaque30@gmail.com>
2023-12-04 13:41:19 -0600

    remove unnecessary import

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import WelcomePage from "./welcome-page";
-import GamePage from "./game-page";
+import ErrorPage from "./error-page";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <WelcomePage />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
